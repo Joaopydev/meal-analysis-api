bd6497acc181ec9cdd341e6e6da8cc7f53a5dcb7
Joao <joao.ribeiro@e-deploy.com.br>
2026-04-01 16:51:06 -0300

    implement function to handle file upload s3 event from aws

--- src/functions/file_upload_event.py
+++ src/functions/file_upload_event.py
@@ -1,23 +1,23 @@
-import asyncio
 import os
+import asyncio
+import json
 
 from dotenv import load_dotenv
-
 from ..app_types.s3_events import S3Event
-from ..clients.sqs_client import sqs_client
+from ..clients.sqs_client import get_sqs_client
 
 load_dotenv()
 
 async def async_handler(event: S3Event):
-    async with sqs_client as client:
+    async with get_sqs_client() as client:
         tasks = [
             client.send_message(
                 QueueUrl=os.getenv("MEALS_QUEUE_URL"),
-                MessageBody=record["s3"]["object"]["key"]
+                MessageBody=json.dumps({"file_key": record["s3"]["object"]["key"]}),
             )
             for record in event["Records"]
         ]
-        return await asyncio.gather(*tasks)
+        await asyncio.gather(*tasks)
 
-def handler(event: S3Event):
-    return asyncio.run(async_handler(event=event))
+def handler(event: S3Event, _):
+    asyncio.run(async_handler(event=event))
