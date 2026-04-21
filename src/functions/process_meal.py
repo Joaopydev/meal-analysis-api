e42bcfa8d3671bedbcc5d1dc4f0baccc84b26955
Joao <joao.ribeiro@e-deploy.com.br>
2026-04-21 11:34:22 -0300

    minor changes

--- src/functions/process_meal.py
+++ src/functions/process_meal.py
@@ -3,10 +3,16 @@ import asyncio
 
 from ..queues.process_meal import ProcessMeal
 from ..repository.meal_repository import MealRepository
+from ..services.ai import AIClient
+from ..services.storage import StorageService
 
 
 async def async_handler(event):
-    process_meal = ProcessMeal(MealRepository())
+    process_meal = ProcessMeal(
+        meal_repository=MealRepository(),
+        storage_service=StorageService(),
+        ai_client=AIClient()
+    )
     tasks = [
         process_meal.process(file_key=json.loads(record["body"])["file_key"])
         for record in event["Records"]
