a0490ae32225a133ce8714d37740aa96b164f48b
Joao <joao.ribeiro@e-deploy.com.br>
2026-04-22 16:38:24 -0300

    minor changes

--- src/services/storage.py
+++ src/services/storage.py
@@ -1,8 +1,10 @@
 import os
-import logging
+from typing import Dict, Any
 from dotenv import load_dotenv
 
 from botocore.exceptions import ClientError
+from botocore.response import StreamingBody
+
 from ..clients.s3_client import s3_client
 
 
@@ -12,11 +14,11 @@ load_dotenv()
 class StorageService:
 
     @classmethod
-    def get_presigned_url(cls, file_key: str, content_type: str, expires_in: int = 600):
+    def get_presigned_url(cls, method_type: str, file_key: str, content_type: str, expires_in: int = 600) -> str:
         # Generate pre-signed URL
         try:
             presigned_url = s3_client.generate_presigned_url(
-                "put_object",
+                method_type,
                 Params={
                     "Bucket": os.getenv("BUCKET_NAME"),
                     "Key": file_key,
@@ -29,7 +31,7 @@ class StorageService:
             raise RuntimeError(f"Error generating presigned URL: {e}") from e
     
     @classmethod
-    def get_object_from_bucket(cls, key: str):
+    def get_object_from_bucket(cls, key: str) -> Dict[str, Any]:
         try:
             return s3_client.get_object(
                 Bucket=os.getenv("BUCKET_NAME"),
@@ -39,6 +41,7 @@ class StorageService:
             raise RuntimeError(f"Failed to fetch object in s3: {e}") from e
         
     @classmethod
-    def read_object_content(cls, key: str):
+    def read_object_content(cls, key: str) -> bytes:
         obj = cls.get_object_from_bucket(key)
-        return obj["Body"].read()
+        streaming_body: StreamingBody = obj["Body"]
+        return streaming_body.read()
